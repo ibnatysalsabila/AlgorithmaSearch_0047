151dee0351b9d5c2405061a2abcd65c722717c08
ibnatysalsabila <salsajpr01@gmail.com>
2024-04-04 18:26:05 +0700

    memasukan void input

--- ActivitySearch/ActivitySearch.cpp
+++ ActivitySearch/ActivitySearch.cpp
@@ -4,3 +4,15 @@ using namespace std;
 int arr[20]; // Array to be searched
 int n; // Number of elements in the array
 int i; // Index of array element
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 dan maximum 20 elements. \n\n";
+	}
